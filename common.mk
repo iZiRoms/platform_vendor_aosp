8dbad9a76f7e80c323d79f364c9942e17f7f17b9
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2017-08-01 19:10:56 +0000

    Use lineage recorder

--- common.mk
+++ common.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
-    SoundRecorder
+    Recorder
 
 # Custom off-mode charger
 ifneq ($(WITH_CM_CHARGER),false)
